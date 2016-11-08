f4d191367c675b8ef09c1ee30f344f463ed24729
Martina <xvitovs1@ad.fi.muni.cz>
2016-11-08 11:44:05 +0100

    Add changes for kd-tree

--- pa093/KdTree.java
+++ pa093/KdTree.java
@@ -15,7 +15,7 @@ public class KdTree{
         splitPoints(l, points, p1, p2);
       } else{
         //TODO check this later
-        Collections.sort(p, new PointsByYComparator());
+        Collections.sort(points, new PointsByYComparator());
         l = points.get(points.size() / 2);
         splitPoints(l, points, p1, p2);
       }
@@ -38,6 +38,32 @@ public class KdTree{
     while(i < points.size()){
       p2.add(points.get(i));
       i++;
+    }
+  }
+  
+  public static ArrayList<LineSegment> getLines(KdNode root, int bottom, int maxRight){
+    return getLines(root, bottom, 0, 0, maxRight);
+  }
+  
+  private static ArrayList<LineSegment> getLines(KdNode root, int bottomLine, int topLine, int leftLine, int rightLine){
+    if(root == null){
+      return new ArrayList<LineSegment>();
+    }
+    
+    if(root.depth % 2 == 0){
+      Point x = new Point(root.point.x, topLine);
+      Point y = new Point(root.point.x, bottomLine);
+      LineSegment l = new LineSegment(x,y);
+      ArrayList<LineSegment> lines = new ArrayList<LineSegment>();
+      lines.add(l);
+      return lines.addAll(getLines(root.left)).addAll(getLines(root.right));
+    } else {
+      Point x = new Point(leftLine,root.point.y);
+      Point y = new Point(rightLine,root.point.y);
+      LineSegment l = new LineSegment(x,y);
+      ArrayList<LineSegment> lines = new ArrayList<LineSegment>();
+      lines.add(l);
+      return lines.addAll(getLines(root.left)).addAll(getLines(root.right));
     }
   }
 }
--- pa093/PointsByXComparator.java
+++ pa093/PointsByXComparator.java
@@ -4,15 +4,7 @@ public class PointsByXComparator implements Comparator<Point> {
   
   @Override
   public int compare(Point a, Point b) {
-    float diff = a.x - b.x;
-    if(diff > 0) return -1;
-    if(diff < 0) return 1;
-    
-    diff = a.y - b.y;
-    if(diff > 0) return -1;
-    if(diff < 0) return 1;
-    
-    return 0;
+    return a.x - b.x;
   }
 
   public boolean equals(Object obj) {
